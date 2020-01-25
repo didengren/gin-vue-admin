a1cbd408e0a1f9c77f222291df527e6fa0f61861
nianzu.xu@trinasolar.com <nianzu.xu@trinasolar.com>
2020-01-25 10:17:40 +0800

    添加src pkg bin目录

---
+++ QMPlusServer/src/cmd/windows.go
@@ -0,0 +1,28 @@
+package cmd
+
+import (
+	"fmt"
+	"gin-vue-admin/config"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func RunWindowsServer(Router *gin.Engine) {
+	s := &http.Server{
+		Addr:           fmt.Sprintf(":%d", config.GinVueAdminconfig.System.Addr),
+		Handler:        Router,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	time.Sleep(10 * time.Microsecond)
+	fmt.Printf(`欢迎使用 Gin-Vue-Admin
+	作者：奇淼 And Spike666
+	微信：shouzi_1994
+	默认自动化文档地址:http://127.0.0.1%s/swagger/index.html
+	默认前端文件运行地址:http://127.0.0.1:8080
+`, s.Addr)
+	_ = s.ListenAndServe()
+}
